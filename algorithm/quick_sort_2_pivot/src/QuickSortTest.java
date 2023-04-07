e8cb6953717b0b0a9364fbf7c5d229532440a40c
mobin <mobinpower007@gmail.com>
2023-04-07 16:38:17 +0430

    add performance tests

--- algorithm/quick_sort_2_pivot/src/QuickSortTest.java
+++ algorithm/quick_sort_2_pivot/src/QuickSortTest.java
@@ -1,9 +1,9 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,9 +11,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class QuickSortTest {
     @Test
-    void f() {
-        List<Comparable<Integer>> l1 = new ArrayList<>(List.of(5, 6, 3, 4, 1, 7, 9));
-        QuickSort.sort(l1);
-        assert (l1.equals(List.of(1, 3, 4, 5, 6, 7, 9)));
+    void simpleSortIntTest() {
+        List<Integer> l = new ArrayList<>(List.of(5, 6, 3, 4, 1, 7, 9));
+        QuickSort.sort(l);
+        assert (l.equals(l.stream().sorted().toList()));
+    }
+
+    @Test
+    void simpleSortDoubleTest() {
+        List<Double> l = new ArrayList<>(List.of(-1.23, -3.23, 1.3, 2.0, 43.3));
+        QuickSort.sort(l);
+        assert (l.equals(l.stream().sorted().toList()));
+    }
+
+    @Test
+    @Timeout(value = 500, unit = TimeUnit.MILLISECONDS)
+    void bigSortIntTest() {
+        Random random = new Random(0);
+        Supplier<Integer> integerSupplier = random::nextInt;
+        List<Integer> l = new ArrayList<>(Stream.generate(integerSupplier).limit(100000).toList());
+        QuickSort.sort(l);
+        assert (l.equals(l.stream().sorted().toList()));
+    }
+    @Test
+    @Timeout(value = 500, unit = TimeUnit.MILLISECONDS)
+    void bigSortDoubleTest() {
+        Random random = new Random(0);
+        Supplier<Double> integerSupplier = random::nextDouble;
+        List<Double> l = new ArrayList<>(Stream.generate(integerSupplier).limit(100000).toList());
+        QuickSort.sort(l);
+        assert (l.equals(l.stream().sorted().toList()));
     }
 }
