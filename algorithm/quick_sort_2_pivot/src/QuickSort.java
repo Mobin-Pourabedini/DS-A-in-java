c99a56e38342ebad17ca50d1d8a679607a8d7edc
mobin <mobinpower007@gmail.com>
2023-04-07 16:35:55 +0430

    adjust type generics of sort function so that it accepts all kind of comparables

--- algorithm/quick_sort_2_pivot/src/QuickSort.java
+++ algorithm/quick_sort_2_pivot/src/QuickSort.java
@@ -3,23 +3,23 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class QuickSort {
-    public static <T> void sort(List<Comparable<T>> list) {
+    public static <T extends Comparable<? super T>> void sort(List<T> list) {
         if (list.size() <= 1) {
             return;
         }
-        if (cmp(list.get(0), list.get(1)) > 0) Collections.swap(list, 0, 1);
+        if (list.get(0).compareTo(list.get(1)) > 0) Collections.swap(list, 0, 1);
 
-        Comparable<T> a = list.get(0), b = list.get(1);
+        T a = list.get(0), b = list.get(1);
 
-        List<Comparable<T>> l1 = new ArrayList<>(),
+        List<T> l1 = new ArrayList<>(),
                 l2 = new ArrayList<>(), l3 = new ArrayList<>();
 
-        for (Comparable<T> comparable : list.subList(2, list.size())) {
-            if (cmp(comparable, a) < 0) {
+        for (T comparable : list.subList(2, list.size())) {
+            if (comparable.compareTo(a) < 0) {
                 l1.add(comparable);
                 continue;
             }
-            if (cmp(comparable, b) < 0) {
+            if (comparable.compareTo(b) < 0) {
                 l2.add(comparable);
                 continue;
             }
@@ -29,12 +29,8 @@ public class QuickSort {
         sort(l1);sort(l2);sort(l3);
         l1.add(a);
         l2.add(b);
-        List<Comparable<T>> list2 = Stream.of(l1, l2, l3).flatMap(Collection::stream).toList();
+        List<T> list2 = Stream.of(l1, l2, l3).flatMap(Collection::stream).toList();
 
         Collections.copy(list, list2);
-    }
-
-    private static <T> int cmp(Comparable<T> comparable1, Comparable<T> comparable2){
-        return comparable1.compareTo((T) comparable2);
     }
 }
